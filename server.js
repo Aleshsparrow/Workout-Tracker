dbea330c7c8823a14775887ecc161aba2d3ed40b
Segun Alesinloye <segunalesh@gmail.com>
2020-02-11 09:33:50 -0600

    Finished setting up server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mongojs = require("mongojs")
 
 const PORT = process.env.PORT || 3000
 
@@ -8,11 +9,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+const databaseUrl = "workout";
+const collections = ["workouts"];
+
+const db = mongojs(databaseUrl, collections);
+
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
+});
+
+db.on("error", error => {
+  console.log("Database Error:", error);
 });
 
 // routes
