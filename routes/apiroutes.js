1fdf538bcee635eeb621425330e1a9d73e0ef39b
Segun Alesinloye <segunalesh@gmail.com>
2020-02-13 10:15:46 -0600

    Got the get api route to work

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,17 +1,20 @@
 const router = require("express").Router();
-const db = require("../models/workout.js")
+const db = require("../models")
 
+// console.log(db);
 
 
 router.post("/api/workouts", (req, res) => {
-    db.workouts.create({}).then(function(dbworkouts){
-        res.json(dbworkouts)
-    }).catch(err => {
-        res.status(400).json(err);
-        });
+    console.log(req.body)
+    // db.workouts.create({}).then(function(dbworkouts){
+        
+    //     res.json(dbworkouts)
+    // }).catch(err => {
+    //     res.status(400).json(err);
+    //     });
 })
 router.get("/api/workouts", (req, res) => {
-    db.workouts.findAll().then(function(dbworkouts){
+    db.Workout.find({}).then(function(dbworkouts){
         res.json(dbworkouts)
     })
     .catch(err => {
